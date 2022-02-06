20ad35bc5758993a014564655ff5e252154ecc17
wasn325 <30980936+wasn325@users.noreply.github.com>
2022-02-06 22:43:45 +0100

    Added Minestom Support for CPerms Module

---
+++ modules/cloudperms/src/main/java/eu/cloudnetservice/modules/cloudperms/minestom/MinestomCloudPermissionsExtension.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019-2022 CloudNetService team & contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cloudnetservice.modules.cloudperms.minestom;
+
+import eu.cloudnetservice.cloudnet.driver.CloudNetDriver;
+import eu.cloudnetservice.cloudnet.wrapper.Wrapper;
+import eu.cloudnetservice.modules.cloudperms.PermissionsUpdateListener;
+import eu.cloudnetservice.modules.cloudperms.minestom.listener.MinestomCloudPermissionsPlayerListener;
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.entity.Player;
+import net.minestom.server.extensions.Extension;
+
+public class MinestomCloudPermissionsExtension extends Extension {
+
+  @Override
+  public void initialize() {
+
+    MinecraftServer.getConnectionManager().getOnlinePlayers()
+        .forEach(MinestomPermissionsHelper::initPlayer);
+
+    new MinestomCloudPermissionsPlayerListener(this, CloudNetDriver.instance().permissionManagement());
+
+    CloudNetDriver.instance().eventManager()
+      .registerListener(
+        new PermissionsUpdateListener<>(
+          runnable -> MinecraftServer.getSchedulerManager().buildTask(runnable).schedule(),
+          MinestomPermissionsHelper::initPlayer,
+          Player::getUuid,
+          MinecraftServer.getConnectionManager()::getPlayer,
+          MinecraftServer.getConnectionManager()::getOnlinePlayers
+        )
+      );
+  }
+
+  @Override
+  public void terminate() {
+    CloudNetDriver.instance().eventManager().unregisterListeners(this.getClass().getClassLoader());
+    Wrapper.instance().unregisterPacketListenersByClassLoader(this.getClass().getClassLoader());
+  }
+}
---
+++ modules/cloudperms/src/main/java/eu/cloudnetservice/modules/cloudperms/minestom/listener/MinestomCloudPermissionsPlayerListener.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2019-2022 CloudNetService team & contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cloudnetservice.modules.cloudperms.minestom.listener;
+
+import eu.cloudnetservice.cloudnet.driver.permission.PermissionManagement;
+import eu.cloudnetservice.modules.cloudperms.CloudPermissionsHelper;
+import eu.cloudnetservice.modules.cloudperms.minestom.MinestomPermissionsHelper;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.event.EventFilter;
+import net.minestom.server.event.EventNode;
+import net.minestom.server.event.player.AsyncPlayerPreLoginEvent;
+import net.minestom.server.event.player.PlayerDisconnectEvent;
+import net.minestom.server.event.player.PlayerLoginEvent;
+import net.minestom.server.event.trait.PlayerEvent;
+import net.minestom.server.extensions.Extension;
+import net.minestom.server.extras.MojangAuth;
+
+public class MinestomCloudPermissionsPlayerListener {
+
+  private final Extension extension;
+  private final PermissionManagement permissionsManagement;
+  private final EventNode<PlayerEvent> node = EventNode.type("cperms", EventFilter.PLAYER);
+
+  public MinestomCloudPermissionsPlayerListener(Extension extension, PermissionManagement permissionsManagement) {
+    this.extension = extension;
+    this.permissionsManagement = permissionsManagement;
+    init();
+  }
+
+  private void init() {
+    node.addListener(AsyncPlayerPreLoginEvent.class, event -> {
+        CloudPermissionsHelper.initPermissionUser(
+          this.permissionsManagement,
+          event.getPlayer().getUuid(),
+          LegacyComponentSerializer.legacySection().serialize(event.getPlayer().getName()),
+          message -> {
+            event.getPlayer().kick(message);
+          },
+          MojangAuth.isEnabled());
+      })
+      .addListener(PlayerLoginEvent.class, event -> {
+        MinestomPermissionsHelper.initPlayer(event.getPlayer());
+      })
+      .addListener(PlayerDisconnectEvent.class, event -> {
+        CloudPermissionsHelper.handlePlayerQuit(this.permissionsManagement, event.getPlayer().getUuid());
+      });
+
+    MinecraftServer.getGlobalEventHandler().addChild(node);
+  }
+
+}
