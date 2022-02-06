a6e042c5e76943d53fbc2acd027f686acbd8aa5c
wasn325 <30980936+wasn325@users.noreply.github.com>
2022-02-06 19:21:37 +0100

    Added Minestom Support for Bridge Module

---
+++ modules/bridge/src/main/java/eu/cloudnetservice/modules/bridge/platform/minestom/MinestomPlayerManagementListener.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2019-2022 CloudNetService team & contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cloudnetservice.modules.bridge.platform.minestom;
+
+import eu.cloudnetservice.cloudnet.wrapper.Wrapper;
+import eu.cloudnetservice.modules.bridge.platform.PlatformBridgeManagement;
+import eu.cloudnetservice.modules.bridge.platform.helper.ServerPlatformHelper;
+import java.util.Locale;
+import lombok.NonNull;
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.event.EventFilter;
+import net.minestom.server.event.EventNode;
+import net.minestom.server.event.player.AsyncPlayerPreLoginEvent;
+import net.minestom.server.event.player.PlayerDisconnectEvent;
+import net.minestom.server.event.trait.PlayerEvent;
+import net.minestom.server.extensions.Extension;
+
+public class MinestomPlayerManagementListener {
+
+  private final Extension extension;
+  private final PlatformBridgeManagement<?, ?> management;
+  private final EventNode<PlayerEvent> node = EventNode.type("bridge", EventFilter.PLAYER);
+
+  public MinestomPlayerManagementListener(@NonNull Extension extension,
+    @NonNull PlatformBridgeManagement<?, ?> management) {
+    this.extension = extension;
+    this.management = management;
+    initListeners();
+  }
+
+  private void initListeners() {
+    node.addListener(AsyncPlayerPreLoginEvent.class, event -> {
+        var task = this.management.selfTask();
+        if (task != null) {
+          // check if the current task is present
+          var player = event.getPlayer();
+          // check if maintenance is activated
+          if (task.maintenance() && !event.getPlayer().hasPermission("cloudnet.bridge.maintenance")) {
+            player.kick(this.management.configuration().message(
+              Locale.forLanguageTag(event.getPlayer().getSettings().getLocale()),
+              "server-join-cancel-because-maintenance"));
+            return;
+          }
+          // check if a custom permission is required to join
+          var permission = task.properties().getString("requiredPermission");
+          if (permission != null && !event.getPlayer().hasPermission(permission)) {
+            player.kick(this.management.configuration().message(
+              Locale.forLanguageTag(event.getPlayer().getSettings().getLocale()),
+              "server-join-cancel-because-permission"));
+            return;
+          }
+        }
+
+        ServerPlatformHelper.sendChannelMessageLoginSuccess(
+          event.getPlayer().getUuid(),
+          this.management.ownNetworkServiceInfo());
+        // update the service info in the next tick
+        MinecraftServer.getSchedulerManager()
+          .buildTask(() -> Wrapper.instance().publishServiceInfoUpdate())
+          .schedule();
+      })
+      .addListener(PlayerDisconnectEvent.class, event -> {
+        ServerPlatformHelper.sendChannelMessageDisconnected(
+          event.getPlayer().getUuid(),
+          this.management.ownNetworkServiceInfo());
+        // update the service info in the next tick
+        MinecraftServer.getSchedulerManager()
+          .buildTask(() -> Wrapper.instance().publishServiceInfoUpdate())
+          .schedule();
+      });
+    MinecraftServer.getGlobalEventHandler().addChild(node);
+  }
+
+}
